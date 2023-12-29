67cf6b52721aa4435a7dfb4bd156fc61298df3b5
Aden <137004239+AdenProSDE2024@users.noreply.github.com>
2023-12-28 21:52:12 -0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,15 +3,19 @@ const fetch = require('node-fetch');
 module.exports = async (req, res) => {
   const { name, suffix } = req.query;
   try {
-    const response = await fetch(`https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`);
-    if (response.ok) {
-      const data = await response.json();
-      res.status(200).json(data);
-    } else {
-      res.status(response.status).send('Error fetching data');
+    const whoisResponse = await fetch(`https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`);
+    
+    // Convert to JSON regardless of response type
+    const data = await whoisResponse.text();
+    try {
+      const jsonData = JSON.parse(data);
+      res.status(whoisResponse.status).json(jsonData);
+    } catch (error) {
+      // If not JSON, send as error message
+      res.status(whoisResponse.status).json({ error: data });
     }
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
